d33a48684e96b5b541460f2f6f735f9ac5d29a4b
rahmate2003 <w.rahmad26@gmail.com>
2024-07-02 15:04:56 +0800

    update

---
+++ src/absensi/Koneksi.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package absensi;
+
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author wrahm
+ */
+
+public class Koneksi {
+    private static java.sql.Connection koneksi;
+    
+    public static java.sql.Connection getKoneksi() {
+        if (koneksi == null) {
+            try{
+                String url = "jdbc:mysql://localhost:3306/absensi";
+                String user = "root";
+                String pass = "";
+                DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+                koneksi = DriverManager.getConnection(url, user, pass);
+                System.out.println("Connection Successfully");
+            }catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("Error");
+            }
+        }
+        return koneksi;
+    }
+    public static void main(String[] args) {
+        getKoneksi();
+    }
+    
+}
